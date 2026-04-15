8797d304abc71ba6d088555d640bcaf4d076c5d3
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-04-15 21:05:43 +0300

    initial commit

--- index.js
+++ index.js
@@ -2,3 +2,61 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+const input = document.getElementById("state-input");
+const button = document.getElementById("fetch-alerts");
+const displayDiv = document.getElementById("alerts-display");
+const errorDiv = document.getElementById("error-message");
+
+if (button) {
+  button.addEventListener("click", () => {
+    fetchWeatherAlerts(input.value);
+  });
+}
+
+function fetchWeatherAlerts(state) {
+  displayDiv.textContent = "";
+  errorDiv.textContent = "";
+  errorDiv.classList.add("hidden");
+
+  if (!state) {
+    showError("Please enter a state");
+    return;
+  }
+
+  fetch(`https://api.weather.gov/alerts/active?area=${state}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Network failure");
+      }
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+
+      displayAlerts(data);
+
+      input.value = "";
+    })
+    .catch(error => {
+      console.log(error.message);
+      showError(error.message);
+    });
+}
+
+function displayAlerts(data) {
+  const alerts = data.features;
+
+  displayDiv.textContent = `Weather Alerts: ${alerts.length}`;
+
+  alerts.forEach(alert => {
+    const p = document.createElement("p");
+    p.textContent = alert.properties.headline;
+    displayDiv.appendChild(p);
+  });
+}
+
+function showError(message) {
+  errorDiv.textContent = message;
+
+  errorDiv.classList.remove("hidden");
+}
